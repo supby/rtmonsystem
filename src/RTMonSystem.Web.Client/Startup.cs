1e3c9629c6e02d0042d82d8f0a0cf0b943766f3f
Andre_Suzanovich <Andre_Suzanovich@epam.com>
2015-03-04 16:46:33 +0300

    add web client

---
+++ src/RTMonSystem.Web.Client/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+[assembly: OwinStartup(typeof(RTMonSystem.Web.Client.Startup))]
+namespace RTMonSystem.Web.Client
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
